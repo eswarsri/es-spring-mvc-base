df8e8c49b630f8bfd399446691fa0dab787377dc
eswaran <eswaran@cordis.us>
2018-03-21 19:24:07 +0530

    init setup

---
+++ src/com/esweb/controller/DefaultController.java
@@ -0,0 +1,21 @@
+package com.esweb.controller;  
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author john
+ */
+@Controller
+public class DefaultController {
+    
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String index(ModelMap map) {
+       map.put("msg", "Hello Spring 4 Web MVC!");
+       return "index";
+   }
+    
+}
